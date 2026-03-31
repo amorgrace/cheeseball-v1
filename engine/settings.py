f2da8fbf21d86ef8d344080a97594aafc4ddaeed
amorgrace <akanforte@gmail.com>
2026-03-31 23:21:17 +0100

    git v2

--- engine/settings.py
+++ engine/settings.py
@@ -36,6 +36,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "cheeseball-v1-qfslq905b-amorgraces-projects.vercel.app",
     "cheeseball-v1-q79qgyku8-amorgraces-projects.vercel.app"
+    "cheeseball-v1-git-master-amorgraces-projects.vercel.app"
 ]
 
 
